0aec574c79717762b2a36fcf02f77ff06a4605d7
Benoit Fraikin <benoit.fraikin@usherbrooke.ca>
2022-05-24 14:27:24 -0400

    Ébauche Soit

--- src/Data/Soit.hs
+++ src/Data/Soit.hs
@@ -1,33 +1,33 @@
-module Data.Peutetre
+module Data.Soit
     where
 
-data Peutetre a = Rien | Juste a
+data Soit r a = Gauche r | Droite a
     deriving (Show, Eq, Read)
 
-fromList :: [a] -> Peutetre a
-fromList []    = Rien
-fromList (x:_) = Juste x
+fromList :: Monoid m => [a] -> Gauche m a
+fromList []    = Gauche mempty
+fromList (x:_) = Droite x
 
-fromMaybe :: Maybe a -> Peutetre a
-fromMaybe Nothing  = Rien
-fromMaybe (Just x) = Juste x
+fromEither :: Either r a -> Soit r a
+fromEither (Left x)  = Gauche x
+fromEither (Right x) = Droite x
 
-instance Semigroup (Peutetre a) where
-    -- (<>) :: Peutetre a -> Peutetre a -> Peutetre a
+instance Semigroup (Soit r a) where
+    -- (<>) :: Soit r a -> Soit r a -> Soit r a
     -- Associativity
     --   (a <> b) <> c == a <> (b <> c)
     (<>) = undefined
 
-instance Monoid (Peutetre a) where
-    -- mempty :: Peutetre a
+instance Monoid (Soit r a) where
+    -- mempty :: Soit r a
     -- Right identity
     --   x <> mempty = x
     -- Left identity
     --   mempty <> x = x
     mempty = undefined
 
-instance Functor Peutetre where
-    -- fmap :: (a -> b) -> Peutetre a -> Peutetre b
+instance Functor (Soit r) where
+    -- fmap :: (a -> b) -> Soit r a -> Soit r b
     -- Identity
     --   fmap id == id
     -- Composition
@@ -39,14 +39,14 @@ instance Functor Peutetre where
     mempty  :: m
     mappend :: m -> m -> m
 -}
-instance Foldable Peutetre where
-    -- foldMap :: Monoid m => (a -> m) -> Peutetre a -> m
+instance Foldable (Soit r) where
+    -- foldMap :: Monoid m => (a -> m) -> Soit r a -> m
     -- Composition of fold and map
     -- with fold = foldMap id
     --   foldMap f = fold . fmap f
     -- this means
     --   foldMap f . fmap g = foldMap (f . g)
-    -- Specifically for Monoid (Peutetre a)
+    -- Specifically for Monoid (Soit r a)
     --   foldMap id ls = fold ls = ls
     foldMap f pe = undefined
 
@@ -55,8 +55,8 @@ instance Foldable Peutetre where
     pure  :: a -> f a
     (<*>) :: f (a -> b) -> f a -> f b
 -}
-instance Traversable Peutetre where
-    -- traverse :: Applicative f => (a -> f b) -> Peutetre a -> f (Peutetre b)
+instance Traversable (Soit r) where
+    -- traverse :: Applicative f => (a -> f b) -> Soit r a -> f (Soit r b)
     -- Naturality
     --   t . traverse f = traverse (t . f)
     --   where t is an applicative transformation
@@ -67,10 +67,10 @@ instance Traversable Peutetre where
     --   traverse (Compose . fmap g . f) = Compose . fmap (traverse g) . traverse f
     traverse f pe = undefined
 
-instance Applicative Peutetre where
-    -- pure  :: a -> Peutetre a
+instance Applicative (Soit r) where
+    -- pure  :: a -> Soit r a
     pure x = undefined
-    -- (<*>) :: Peutetre (a -> b) -> Peutetre a -> Peutetre b
+    -- (<*>) :: Soit r (a -> b) -> Soit r a -> Soit r b
     -- Identity
     --   pure id <*> v = v
     -- Composition
@@ -81,10 +81,10 @@ instance Applicative Peutetre where
     --   u <*> pure y = pure ($ y) <*> u
     (<*>) = undefined
 
-instance Monad Peutetre where
-    -- return :: a -> Peutetre a
+instance Monad (Soit r) where
+    -- return :: a -> Soit r a
     return = undefined
-    -- (>>=)  :: Peutetre a -> (a -> Peutetre b) -> Peutetre b
+    -- (>>=)  :: Soit r a -> (a -> Soit r b) -> Soit r b
     -- Left identity
     --   return a >>= k = k a
     -- Right identity
