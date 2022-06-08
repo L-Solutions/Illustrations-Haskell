f7a754d83006e3705be814a8f5228fa42065b12b
Benoit Fraikin <benoit.fraikin@usherbrooke.ca>
2022-06-08 15:39:43 -0400

    ajout option de l'exemple dans le main

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@
 module Main where
 
 import           Data.Text                               (Text (..))
-import           Data.Text.Utils                         (eol, putText)
+import           Data.Text.Utils
 import qualified Example.Download                        as Ex1 (main)
 import qualified Example.TreeTraverse                    as Ex2 (main)
 import qualified Example.TwoMonadTransformerForSameMonad as Ex3 (main)
@@ -18,10 +18,18 @@ entête =  "#######"                                                         <> 
        <> "#         #  #   #       #    #  #       #       #       #    #" <> eol
        <> "#######  #    #  ######  #    #  #       ######  ######   ####"  <> eol
 
+liste :: [ IO () ]
+liste =  [ Ex1.main, Ex2.main, Ex3.main]
+
 main :: IO ()
 main = do putText entête
           putText eol
-          Ex1.main
-          Ex2.main
-          Ex3.main
+          entier <- getLine
+          let n = read entier
+              numéro = n - 1
+          putTextLn $ "Exemple : " <> pack (show numéro)
+          --
+          liste !! numéro
+          --
+          putTextLn "Fin."
 
